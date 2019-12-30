f0d49faf3d9f3b2604b12b47728284e5f3f01cf7
doublekk <2563983236@qq.com>
2019-12-30 17:28:51 +0800

    my test laravel_project

---
+++ routes/Admin/AdminRoute.php
@@ -0,0 +1,12 @@
+<?php
+Route::get('/aaa',function(){
+
+	return ['a','b','c'];
+
+});
+
+Route::get('/user',function (){
+    $user = DB::table('users')->paginate(5)->links();
+//    $user = $user->appends(['sort' => 'notes']);
+    return $user;
+});
