70a324ffd47b10d88654a79e12f742bd3413bbcb
doublekk <2563983236@qq.com>
2019-12-31 17:02:22 +0800

    login

--- routes/Admin/AdminRoute.php
+++ routes/Admin/AdminRoute.php
@@ -9,6 +9,7 @@ Route::get('/user',function (){
     $user = DB::table('users')->paginate(5)->links();
 //    $user = $user->appends(['sort' => 'notes']);
     return $user;
+    
 });
 
 // Route::get('admin/login','admin/LoginController@index');
